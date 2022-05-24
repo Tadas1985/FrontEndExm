f9cb3548cabf1c9224132b57699ed132696436d9
Tadas <tadas1985@gmail.com>
2022-05-24 18:19:05 +0300

    testing

--- RegistrationForm/registrationForm.js
+++ RegistrationForm/registrationForm.js
@@ -14,28 +14,38 @@
 //     console.log(err);
 //   });
 
-fetch("https://testapi.io/api/Tadas/resource/exam", {
-  method: "POST",
-  headers: {
-    "Content-type": "application/json",
-  },
-  body: JSON.stringify({
-    name: "TestName",
-    lastName: "TestLastName",
-    email: "testEmail",
-  }),
-})
-  .then((Response) => {
-    if (Response.ok) {
-      console.log("ok");
-      return Response.json();
-    } else {
-      console.log("not okay");
+//const { result } = require("cypress/types/lodash");
+
+// fetch("https://testapi.io/api/Tadas/resource/exam", {
+//   method: "POST",
+//   headers: {
+//     "Content-type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "TestName",
+//     lastName: "TestLastName",
+//     email: "testEmail",
+//   }),
+// })
+//   .then((Response) => {
+//     if (Response.ok) {
+//       console.log("ok");
+//       return Response.json();
+//     } else {
+//       console.log("not okay");
+//     }
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+fetch("https://testapi.io/api/Tadas/resource/exam")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
     }
   })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then((result) => console.log(result.data));
