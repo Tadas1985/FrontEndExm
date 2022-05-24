862826cfda3ac52b0aa09fa504d874ffc81b7aca
Tadas <tadas1985@gmail.com>
2022-05-24 21:31:46 +0300

    registration submit

--- RegistrationForm/registrationForm.js
+++ RegistrationForm/registrationForm.js
@@ -1,51 +1,38 @@
-// fetch("https://testapi.io/api/Tadas/resource/exam")
-//   .then((Response) => {
-//     if (Response.ok) {
-//       console.log("ok");
-//       return Response.json();
-//     } else {
-//       console.log("not okay");
-//     }
-//   })
-//   .then((result) => {
-//     console.log(result.data);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+let registrationSubmit = document.querySelector("#submit");
 
-//const { result } = require("cypress/types/lodash");
-
-// fetch("https://testapi.io/api/Tadas/resource/exam", {
-//   method: "POST",
-//   headers: {
-//     "Content-type": "application/json",
-//   },
-//   body: JSON.stringify({
-//     name: "TestName",
-//     lastName: "TestLastName",
-//     email: "testEmail",
-//   }),
-// })
-//   .then((Response) => {
-//     if (Response.ok) {
-//       console.log("ok");
-//       return Response.json();
-//     } else {
-//       console.log("not okay");
-//     }
-//   })
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-fetch("https://testapi.io/api/Tadas/resource/exam")
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
+registrationSubmit.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let fname = document.querySelector("#fname").value;
+  let lName = document.querySelector("#lname").value;
+  let emailAddress = document.querySelector("#email").value;
+  fetch("https://testapi.io/api/Tadas/resource/exam", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name: fname,
+      lastName: lName,
+      email: emailAddress,
+    }),
   })
-  .then((result) => console.log(result.data));
+    .then((response) => {
+      if (response.ok) {
+        console.log("ok");
+        return response.json();
+      } else {
+        console.log("not okay");
+      }
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  BackToIndex();
+});
+
+function BackToIndex() {
+  window.location.href = "/LandingPage/Index.html";
+}
